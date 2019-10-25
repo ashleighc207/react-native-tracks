eff389c4b3f453c14f4a641ad9e60477f791882e
Ashleigh Christopher <ashleighc207@gmail.com>
2019-10-25 16:40:26 -0400

    add in styles for tracks screen:

--- src/screens/tracks/TrackListScreen.js
+++ src/screens/tracks/TrackListScreen.js
@@ -1,17 +1,59 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  Button,
+  FlatList
+} from "react-native";
+import styles from "../../styles/TrackListStyles";
 
 const TrackListScreen = ({ navigation }) => {
+  const data = [
+    {
+      name:
+        "test wtgw dg sdg sdg sd g sdg d gsd fgs fdg dsfg sd fg sdfg dgf dfs g sdfg sdf g"
+    },
+    {
+      name:
+        "test wtgw dg dg sdg sd g sdg d gsd fgs fdg dsfg sd fg sdfg dgf dfs g sdfsg sdf g"
+    },
+    {
+      name:
+        "test wtgws dg dg sdg sd g sdg d gsd fgs fdg dsfg sd fg sdfg dgf dfs g sdfg sdf g"
+    },
+    {
+      name:
+        "tesst wtgw dg dg sdg sd g sdg d gsd fgs fdg dsfg sd fg sdfg dgf dfs g sdfg sdf g"
+    },
+    {
+      name:
+        "test wtgsw dg dg sdg sd g sdg d gsd fgs fdg dsfg sd fg sdfg dgf dfs g sdfg sdf g"
+    }
+  ];
   return (
-    <>
-      <Button
-        title="Go to Track Detail"
-        onPress={() => navigation.navigate("TrackDetail")}
-      />
-    </>
+    <View style={styles.mainContainer}>
+      <Text style={styles.heading}>Your Tracks</Text>
+      <ScrollView style={{ flex: 1 }}>
+        <FlatList
+          data={data}
+          keyExtractor={data => data.name}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity
+                style={styles.track}
+                onPress={() => navigation.navigate("TrackDetail")}
+              >
+                <Text style={styles.trackName}>{item.name}</Text>
+              </TouchableOpacity>
+            );
+          }}
+        ></FlatList>
+      </ScrollView>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default TrackListScreen;
---
+++ src/styles/TrackListStyles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    marginHorizontal: 40,
+    flex: 1
+  },
+  heading: {
+    alignSelf: "center",
+    fontSize: 40,
+    fontWeight: "300",
+    color: "rgba(51,165,162,1.0)",
+    marginTop: 40,
+    marginBottom: 40
+  },
+  track: {
+    paddingVertical: 20,
+    borderBottomWidth: 0.5,
+    borderBottomColor: "rgba(51,165,162,1.0)",
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1
+  },
+  trackName: {
+    flexShrink: 1,
+    flex: 1,
+    fontSize: 18,
+    fontWeight: "300",
+    letterSpacing: 1.1,
+    color: "#424242"
+  }
+});
+
+export default styles;
