caa0b567184eaa405ad6d2d33708fb99762efe69
Ashleigh Christopher <ashleighc207@gmail.com>
2019-10-25 15:41:31 -0400

    add in flows for screen navigations

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import AccountScreen from "./src/screens/tracks/AccountScreen";
+import TrackCreateScreen from "./src/screens/tracks/TrackCreateScreen";
+import TrackDetailScreen from "./src/screens/tracks/TrackDetailScreen";
+import TrackListScreen from "./src/screens/tracks/TrackListScreen";
+import SigninScreen from "./src/screens/auth/SigninScreen";
+import SignupScreen from "./src/screens/auth/SignupScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen
+  }),
+  authenticatedFlow: createBottomTabNavigator({
+    TrackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen
+    }),
+    Account: AccountScreen,
+    TrackCreate: TrackCreateScreen
+  })
 });
+
+export default createAppContainer(switchNavigator);
--- src/screens/auth/SignupScreen.js
+++ src/screens/auth/SignupScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Button, TextInput } from "react-native";
 
-const SignupScreen = () => {
-  return <Text> Signup Screen </Text>;
+const SignupScreen = ({ navigation }) => {
+  return (
+    <>
+      <Text> Sign up screen </Text>
+      <Button
+        title="Go to Signin"
+        onPress={() => navigation.navigate("Signin")}
+      />
+      <Button
+        title="Go to Authenticated Flow"
+        onPress={() => navigation.navigate("authenticatedFlow")}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
